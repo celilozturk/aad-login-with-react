94e2fd1d7d5f898f4308e0b323a2d10abd5239a8
Celil Ozturk <celil.ozturk@windowslive.com>
2023-08-01 19:03:15 +0200

    Config file create, azure msal implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import {config} from './Config';
+import { PublicClientApplication } from '@azure/msal-browser';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    // const [userAccount,setUserAccount]=useState();
+
+    super(props);
+    this.state = {
+      error: null,
+      isAuthenticated: false,
+      user: {}
+    };
+    this.userAccount={
+      user:{}
+    };
+
+    this.login = this.login.bind(this)
+    this.PublicClientApplication = new PublicClientApplication({
+      auth: {
+        clientId: config.appId,
+        redirectUri: config.redirectUri,
+        authority: config.authority
+      },
+      cache: {
+        cacheLocation: "sessionStorage",
+        storeAuthStateInCookie: true
+      }
+    });
+  }
+  async login(){
+
+    try{
+      await this.PublicClientApplication.loginPopup({
+        scopes:config.scopes,
+        prompt:"select_account",
+
+        // redirectUri:"http://localhost:3000/blank.html"
+
+
+      }).then((response)=>{
+        this.PublicClientApplication.setActiveAccount(response.account);
+      });
+
+  //    console.log(this.PublicClientApplication.getAllAccounts().map((x)=>(x.name)))
+       this.userAccount.user= this.PublicClientApplication.getActiveAccount();
+      this.state.user= this.PublicClientApplication.getActiveAccount();
+      console.log(this.userAccount.user);
+   //   console.log(this.PublicClientApplication.getActiveAccount())
+      this.setState({isAuthenticated:true})
+    }
+    catch(err){
+      this.setState({
+        isAuthenticated:false,
+        user:{},
+        error:err
+      });
+    }
+
+
+  }
+  logout(){
+    this.PublicClientApplication.logout();
+  }
+
+  render(){
+
+    return (
+      <div className='App'>
+        <header className='header'>
+          {
+          this.state.isAuthenticated ? <p >Successfully logged in <strong> {this.userAccount.user.username}</strong></p> :
+          <p>
+            <button onClick={()=>this.login()} >Login </button>
+          </p>
+          }
+
+        </header>
+      </div>
+    )
+  }
+
+
+
 }
 
 export default App;
