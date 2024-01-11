b1cb932365d5e970bbd6db401c031039a5cf1236
Celil ÖZTÜRK <celil.ozturk@windowslive.com>
2024-01-11 19:54:17 +0300

    Update Config.js
    changed config and credentials

--- src/Config.js
+++ src/Config.js
@@ -1,9 +1,9 @@
 export const config={
-    appId:'d7817699-bc8e-4f37-8a52-5c64bff11327',
-    redirectUri:'https://reactappexample.azurewebsites.net',
+    appId:'',
+    redirectUri:'https://reactappexampletest2023.azurewebsites.net',
     scopes:[
        'user.read'
     ],
-    authority:'https://login.microsoftonline.com/celilozturkwindowslive.onmicrosoft.com'
+    authority:'creadentials for login domain'
 };
 
